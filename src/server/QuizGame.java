de0598049b77b7b21f17eafa877f40f0d95b38d3
Pontus Häggblom <pbhaggblom@gmail.com>
2024-11-23 19:35:45 +0100

    Lägger till ett "protokoll" för att kunna spela med rätt antal rundor och frågor.

--- src/server/QuizGame.java
+++ src/server/QuizGame.java
@@ -9,8 +9,8 @@ public class QuizGame extends Thread {
     private final Player player1;
     private final Player player2;
     private Player activePlayer;
-    private int numOfRounds;
-    private int numOfQuestionsPerRound;
+    private final int numOfRoundsPerGame;
+    private final int numOfQuestionsPerRound;
 
     public QuizGame(Player player1, Player player2) {
         this.player1 = player1;
@@ -22,12 +22,16 @@ public class QuizGame extends Thread {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        numOfRounds = Integer.parseInt(p.getProperty("numOfRounds", "3"));
+        numOfRoundsPerGame = Integer.parseInt(p.getProperty("numOfRoundsPerGame", "3"));
         numOfQuestionsPerRound = Integer.parseInt(p.getProperty("numOfQuestionsPerRound", "3"));
     }
 
     @Override
     public void run() {
+
+        int rounds = 0;
+        int questions = 0;
+        int numOfPlayersAnswered = 0;
 
         System.out.println(player1.getName() + " " + player1.receive());
         System.out.println(player2.getName() + " " + player2.receive());
@@ -35,20 +39,42 @@ public class QuizGame extends Thread {
         activePlayer = player1;
         activePlayer.send("CATEGORY");
         String input;
-        while (true) {
-            //välj kategori
-            //svara på frågor
-            //spara resultat
 
+        while (true) {
             try {
                 input = activePlayer.receive();
-                if (input.startsWith("category chosen")) {
+                if (input.startsWith("chosen category")) {
                     System.out.println(activePlayer.getName() + " " + input);
                     activePlayer.send("QUESTION");
-                } else if (input.startsWith("question answered")) {
-                    System.out.println(activePlayer.getName() + " " + input);
-                    changeActivePlayer();
-//                    activePlayer.send("CATEGORY");
+                } else if (input.startsWith("answered")) {
+                    questions++;
+                    if (questions < numOfQuestionsPerRound) {
+                        System.out.println(activePlayer.getName() + " " + input);
+                        System.out.println("Questions: " + questions);
+                        activePlayer.send("QUESTION");
+                    } else {
+                        System.out.println(activePlayer.getName() + " " + input);
+                        System.out.println("Questions: " + questions);
+                        numOfPlayersAnswered++;
+                        if (numOfPlayersAnswered == 2) {
+                            rounds++;
+                            System.out.println("Rounds: " + rounds);
+                            if (rounds < numOfRoundsPerGame) {
+                                questions = 0;
+                                numOfPlayersAnswered = 0;
+                                activePlayer.send("CATEGORY");
+                            } else {
+                                rounds = 0;
+                                player1.send("RESULT");
+                                player2.send("RESULT");
+                            }
+                        } else {
+                            questions = 0;
+                            activePlayer.send("WAIT");
+                            activePlayer = activePlayer.getOpponent();
+                            activePlayer.send("QUESTION");
+                        }
+                    }
                 }
             } catch (RuntimeException e) {
                 System.out.println("Active player disconnected");
