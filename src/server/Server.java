adc1cc5220bb46a48369a0759ad87ea0043e0f7c
Pontus Häggblom <pbhaggblom@gmail.com>
2024-11-28 15:50:42 +0100

    Fixar så man kan lägga till användarnamn

--- src/server/Server.java
+++ src/server/Server.java
@@ -2,6 +2,7 @@ package server;
 
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.util.List;
 
 public class Server {
 
@@ -9,9 +10,9 @@ public class Server {
         try (ServerSocket ss = new ServerSocket(55555)) {
             while (true) {
                 Player player1 = new Player(ss.accept(), "Player 1");
-                player1.send(new Response("WAIT", null));
+                player1.send(new Response("WAIT", List.of(player1.getName())));
                 Player player2 = new Player(ss.accept(), "Player 2");
-                player2.send(new Response("WAIT", null));
+                player2.send(new Response("WAIT", List.of(player2.getName())));
                 player1.setOpponent(player2);
                 player2.setOpponent(player1);
                 QuizGame newGame = new QuizGame(player1, player2);
